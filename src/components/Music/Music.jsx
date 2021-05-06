ea7580ad4fe956e85151404392ed4e665e948f73
Jchenhall <chenhall_j@hotmail.co.uk>
2021-05-06 14:19:13 +0100

    changed default album art and added scrolling album selection

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 import backdrop from '../../assets/images/artcover.png';
 import movingOn from '../../assets/images/movingOn.png';
 import whereAreYou from '../../assets/images/whereAreYou.jpg';
-
-
+import defaultAlbum from '../../assets/images/defaultAlbum.png';
+import logoBlue from '../../assets/images/blueStrips.png'
 
 const Music = () => {
     return (
@@ -16,10 +16,20 @@ const Music = () => {
 <Row className="musicRow" style={{ paddingLeft: 0, paddingRight: 0 }}>
     <Col md={9} className="mainMusic" style={{ paddingLeft: 0, paddingRight: 0 }}>
         <img src={backdrop} className="backdrop"/>
-        <img src={whereAreYou} className="displayAlbum"/>
+        <img src={defaultAlbum} className="displayAlbum"/>
     </Col> 
     <Col md={3} className="sideBar" style={{ paddingLeft: 0, paddingRight: 0 }}>
-        <div>album</div>
+        
+    <img src={logoBlue} className="logoMusic"/>
+
+        <h1> Search for EP here</h1>
+
+        <div className="albumDiv">
+        <img src={movingOn} className="albumList"/>
+            <img src={whereAreYou} className="albumList"/>
+            <img src={movingOn} className="albumList"/>
+            <img src={whereAreYou} className="albumList"/>
+        </div>
     </Col> 
 </Row>
 
