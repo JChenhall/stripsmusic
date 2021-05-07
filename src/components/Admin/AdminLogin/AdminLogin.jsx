18162a69286bf7842646c3fb1d5fbc52f765392e
Jchenhall <chenhall_j@hotmail.co.uk>
2021-05-07 15:03:12 +0100

    finished stage 1 for music page added react hook form and made admin login page

--- src/components/Admin/AdminLogin/AdminLogin.jsx
+++ src/components/Admin/AdminLogin/AdminLogin.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+
+import './AdminLogin.css';
+import {useForm } from 'react-hook-form';
+import { ErrorMessage } from "@hookform/error-message";
 
 const AdminLogin = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        criteriaMode: "all"
+      });
+    const onSubmit = data => console.log(data);
+
     return (
-        <div>
-            ADMIN LOGIN
-        </div>
+        <section className= "adminBackground">
+        <form onSubmit={handleSubmit(onSubmit)} className="loginForm">
+            <div className="inputBox">
+            <h2> Admin Portal</h2>
+
+            <input 
+            type="text"
+            placeholder="  username"
+            name="username"
+            className= "logLine"
+            {...register("username",  
+            { required: true,  maxLength: 20 })} 
+            />
+            
+            <input type="text"
+            name="details"
+            {...register("details", { required: <p className="loginAlert">"Please fill in both fields"</p> })} 
+            placeholder="  password"
+            className= "logLine"
+            
+            />
+
+            <ErrorMessage
+                errors={errors}
+                name="details"
+                render={({ messages }) => {
+                console.log("messages", messages);
+                return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                <p key={type}>{message}</p>
+              ))
+            : null;
+        }}
+      />
+            <p>forgotten password</p>
+
+            <input type="submit" className="adminSubmit"/>
+            </div>
+        </form>
+            
+        </section>
     )
 }
 
--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Music.css';
 import GoogleFontLoader from 'react-google-font-loader';
 import {Container, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+
 import backdrop from '../../assets/images/artcover.png';
 import movingOn from '../../assets/images/movingOn.png';
 import whereAreYou from '../../assets/images/whereAreYou.jpg';
@@ -15,24 +15,25 @@ const Music = () => {
         <GoogleFontLoader fonts={[{font: 'Roboto',weights: [400, '400i'],},{font: 'Roboto Mono',weights: [400, 700],},]}subsets={['cyrillic-ext', 'greek']}/>
 <Row className="musicRow" style={{ paddingLeft: 0, paddingRight: 0 }}>
     <Col md={9} className="mainMusic" style={{ paddingLeft: 0, paddingRight: 0 }}>
-        <img src={backdrop} className="backdrop"/>
-        <img src={defaultAlbum} className="displayAlbum"/>
+        <img src={backdrop} alt="backdrop" className="backdrop"/>
+        <img src={defaultAlbum} alt="defaultalbum" className="displayAlbum"/>
     </Col> 
     <Col md={3} className="sideBar" style={{ paddingLeft: 0, paddingRight: 0 }}>
-        
-    <img src={logoBlue} className="logoMusic"/>
-
-        <h1> Search for EP here</h1>
-
-        <div className="albumDiv">
-        <img src={movingOn} className="albumList"/>
-            <img src={whereAreYou} className="albumList"/>
-            <img src={movingOn} className="albumList"/>
-            <img src={whereAreYou} className="albumList"/>
+        <div className="logoBox">
+            <img src={logoBlue} alt="logo" className="logoMusic"/>
         </div>
+        <h3 className="searchHeader"> Search for EP here</h3>
+        <div className="searchBarDiv">
+        <input type="text" placeholder="  search for an EP" className="searchBar"/>
+        </div>
+            <div className="albumDiv">
+            <img src={movingOn} alt="album list" className="albumList"/>
+                <img src={whereAreYou} alt="whereAreYou" className="albumList"/>
+                <img src={movingOn} alt="movingOn" className="albumList"/>
+                <img src={whereAreYou} alt="whereAreYou" className="albumList"/>
+            </div>
     </Col> 
 </Row>
-
 </Container>      
     )
 }
