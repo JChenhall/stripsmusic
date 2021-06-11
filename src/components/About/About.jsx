d302658df7de2f250a8586a9e29e611acf8c9e10
Jchenhall <chenhall_j@hotmail.co.uk>
2021-06-11 16:13:14 +0100

    about text added to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            ABOUT
+           <h1>ABOUT</h1> 
+           <p>First draft of About/History info for our website for Jack:
+
+We started out playing covers in a pub band in Norwich. Then punk kicked open the doors and we started writing songs, found Lyndall, then Paul, and called ourselves Rumble Strips.
+
+After a couple of months we made a demo cassette and caught the ear of Chrysalis Records, who paid for a recording session at Konk for us to showcase new songs.
+
+Racing Cars – a Chrysalis act – took us on their big college tour as support and we ended up at the Hammersmith Odeon in October '77.
+
+We played all over the UK, slept in the van if we had to, driving back to Norwich when we could, and recorded tracks for a John Peel session in spring '78.
+
+Fashions change. Powerpop, skinny ties, New Romantics, the 80's, decade of the synthesiser. We all moved on.
+
+Now we're making new music.
+</p>
         </div>
     )
 }
